8630c7c1ab13d49414831080b0ef6d8894a96d9f
JLemus0000 <jlemus0000@gmail.com>
2017-03-13 15:40:42 -0600

    Modificaciones branch b

--- app/src/main/java/firebasetest/brio/adolfo1295/com/myapplication/ActivityB.java
+++ app/src/main/java/firebasetest/brio/adolfo1295/com/myapplication/ActivityB.java
@@ -2,8 +2,11 @@ package firebasetest.brio.adolfo1295.com.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import butterknife.BindView;
 
 /**
  * Created by adolfo1295 on 13/03/17.
@@ -11,15 +14,27 @@ import android.widget.Toast;
 
 public class ActivityB extends AppCompatActivity {
 
+    @BindView(R.id.btnMasterB)
+    Button masterB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_b);
 
        //Activity created n branch-b
-        methodOnB("Hi B");
-
+        setActions();
     }
+
+    private void setActions(){
+        masterB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodOnB("Hi B");
+            }
+        });
+    }
+
 
     private void methodOnB(String message){
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
