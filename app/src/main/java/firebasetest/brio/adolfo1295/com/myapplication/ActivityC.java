89343f7003a852823cecb7ec033e2765150dc831
JLemus0000 <jlemus0000@gmail.com>
2017-03-13 15:48:27 -0600

    Cambios en branch C

--- app/src/main/java/firebasetest/brio/adolfo1295/com/myapplication/ActivityC.java
+++ app/src/main/java/firebasetest/brio/adolfo1295/com/myapplication/ActivityC.java
@@ -2,23 +2,40 @@ package firebasetest.brio.adolfo1295.com.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import butterknife.BindView;
 
 /**
  * Created by adolfo1295 on 13/03/17.
  */
 
 public class ActivityC extends AppCompatActivity {
+
+    @BindView(R.id.btnMasterC)
+    Button btnMasterC;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_c);
 
         //Activity created on branch-c
-        methodOnC("Hi C");
+        setActions();
 
     }
+
+    private void setActions(){
+        btnMasterC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodOnC("Hi C");
+            }
+        });
+    }
+
     private void methodOnC(String messageC){
         Toast.makeText(getApplicationContext(), messageC, Toast.LENGTH_SHORT).show();
     }
